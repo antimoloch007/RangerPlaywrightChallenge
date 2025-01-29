3ae7874057df9ebfb787ea6f6cd309f27ce7b49a
antimoloch007 <007@antimoloch.xyz>
2025-01-29 11:45:24 -0800

    fixing add tests with assertions

--- tests/addTodos.ts
+++ tests/addTodos.ts
@@ -1,4 +1,5 @@
 import { test, expect, Page } from '@playwright/test';
+import { assert } from 'console';
 
 export async function run(page: Page, params) {
     await page.setViewportSize({ width: 1440, height: 1080 });
@@ -14,6 +15,11 @@ export async function run(page: Page, params) {
     await page.keyboard.type('buy apples', { delay: 50 });
     await page.keyboard.press('Enter');
 
+    // Make sure the todo item is added for apples and that 1 items is left
+    await expect(page.getByTestId('todo-title')).toHaveCount(1);
+    await expect(page.getByTestId('todo-count')).toHaveText('1 item left');
+    
+
     //// PLAYWRIGHT_STEP_START id=67410d4c3c095c7f492df999
     ////      Description: Type 'clean house' on input field with placeholder 'What needs to be done?'
 
@@ -22,6 +28,12 @@ export async function run(page: Page, params) {
     await todoInputField2.click();
     await page.keyboard.type('clean house', { delay: 50 });
     await page.keyboard.press('Enter');
+
+    // Make sure the todo item is added for clean house and that 2 items is left
+    await expect(page.getByTestId('todo-title')).toHaveCount(2);
+    await expect(page.getByTestId('todo-count')).toHaveText('2 items left');
+
+
 
     //// PLAYWRIGHT_STEP_START id=67410d693c095c7f492df99a
     ////      Description: Type todo item on input field with placeholder 'What needs to be done?'
@@ -32,6 +44,10 @@ export async function run(page: Page, params) {
     await page.keyboard.type('send package', { delay: 50 });
     await page.keyboard.press('Enter');
 
+    // Make sure the todo item is added for send package and that 3 items is left
+    await expect(page.getByTestId('todo-title')).toHaveCount(3);
+    await expect(page.getByTestId('todo-count')).toHaveText('3 items left');
+
     //// PLAYWRIGHT_STEP_START id=67410d843c095c7f492df99b
     ////      Description: // Type 'mow lawn' on input field with placeholder 'What needs to be done?'
 
@@ -40,4 +56,9 @@ export async function run(page: Page, params) {
     await todoInputField4.click();
     await page.keyboard.type('mow lawn', { delay: 50 });
     await page.keyboard.press('Enter');
+
+    // Make sure the todo item is added for mow lawn and that 4 items is left
+    await expect(page.getByTestId('todo-title')).toHaveCount(4);
+    await expect(page.getByTestId('todo-count')).toHaveText('4 items left');
+
 }
